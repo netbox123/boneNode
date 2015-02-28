5609a901f586e59be4285f018912d398bc5731ba
netbox123 <martijnheeroma@me.com>
2015-02-28 12:04:19 +0100

    Initial Commit

---
+++ OSX client/BoneNode/BoneNode/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BoneNode
+//
+//  Created by Martijn Heeroma on 28-02-15.
+//  Copyright (c) 2015 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ OSX client/BoneNode/BoneNode/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  BoneNode
+//
+//  Created by Martijn Heeroma on 28-02-15.
+//  Copyright (c) 2015 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
